5a216054f79d8cd336f2f7aa52cad8d04c076b14
Jason Boerner <jasonboerner@Jasons-MBP.T-mobile.com>
2019-03-25 14:11:28 -0400

    adds conn.js file

--- examples.sql
+++ examples.sql
@@ -1,20 +1,34 @@
 -- user profile
     -- 1. get all columns for a user by id #
     select * from users
-    where user_id = 1;
+        where id = 1;
     --  1a. get only a few fields for pbulic version
     select first_name, email from users;
     --  1b. get all fields for private version
     select * from users;
     -- 2. get all favorites by a user by id #
     select * from favorites
-    where user_id = 2;
+        where user_id = 2;
     -- 3. get all reviews written by that user by id #
+    select * from reviews 
+        where user_id = 1;
 
 -- restaurant profile
     -- 1. get all info for a restaurant by id #
+    select * from restaurants;
     -- 2. get all reviews for restaurant by id #
+    select * from reviews 
+        where restaurant_id = 1;
     -- 3. get avarage review for a restaurant by id #
+    select name rev.name, ave(rev.score), count(fav.user_id)
+        from reviews rev
+        inner join reviews rev
+            on rev.id = rev.restaurant_id
+        inner join favorites fav
+            on rev.id = fav.restaurant_id
+            where rev.name ilike "applebees"
+            group by res.name;
+
     -- 4. get count of favorites for restaurant by id #
     -- 
 
