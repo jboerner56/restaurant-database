2034115f612286ea1d1bb05ee4d06bdddf6e8c83
Jason Boerner <jasonboerner@Jasons-MBP.T-mobile.com>
2019-03-26 11:40:46 -0400

    adds user instance and test capibility

--- test/test.js
+++ test/test.js
@@ -1,7 +1,21 @@
-const assert = require('assert');
+// const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+const User = require('../models/users');
+chai.use(chaiAsPromised).should();
+// check to make sure mocha is working
+// describe('sanity Check', function(){
+//     it('should be 2', function (){
+//         // assert.equal(2, 1 + 1);
+//         expect(1 + 1).to.equal(2);
+//     });
+// });
 
-describe('sanity Check', function(){
-    it('should be 2', function (){
-        assert.equal(2, 1 + 1);
+describe('Users model', () => {
+    it('should be able to retrieve by id', async () => {
+        const theUser = await User.getById(3);
+        theUser.should.be.an.instanceOf(User);
+        // theUser.should.have.length(1);
     });
-});
+}); 
