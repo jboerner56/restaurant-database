75a43935310621b54d6ab5227387d0e37e123229
Jason Boerner <jasonboerner@Jasons-MBP.T-mobile.com>
2019-03-28 10:55:23 -0400

    adds getting/displaying all reviews in browser

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
+// import reviews class
+const Review = require('./models/reviews');
 //  helper fucnction == "middle-ware" or "request handlers"
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     res.statusCode = 200;
-    res.setHeader('content-type', 'text/plain');
-    res.end('hello there');
+    res.setHeader('content-type', 'application.json');
+    // gets all the reviews from tables
+    const allReviews = await Review.getAll();
+    // needs to be turned into a string so the browser can display it
+    const allReviewsJSON = JSON.stringify(allReviews)
+    res.end(allReviewsJSON);
 });
 
 server.listen(port, hostname, () => {
